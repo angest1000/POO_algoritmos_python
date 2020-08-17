5e6a352c6a4fadd555b243422eca40b31fc48e72
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-16 19:17:11 -0500

    Implementamos al código hasta la parte de PROPIEDADES del blog

--- clases_metodos_prop.py
+++ clases_metodos_prop.py
@@ -2,36 +2,76 @@
 # https://recursospython.com/guias-y-manuales/clases-metodos-magicos-y-propiedades/
 
 #Ejemplo:
+from functools import wraps
 
-class Time:
+def _int_requerido(f):
+    @wraps(f)
+    def wrapper(self, value):
+        if not isinstance(value,int):
+            raise TypeError('Se requiere un valor int')
+        return f(self,value)
+    return wrapper
+
+def _balance(a,b):
+    if b >= 0:
+        while b >=60:
+            a += 1
+            b -= 60
+    elif b < 0:
+        while b < 0:
+            a -= 1
+            b += 60
+    return a,b
+
+class Tiempo: #Horas, minutos, segundos
     def __init__(self,h=0,m=0,s=0):
         self.h = h
         self.m = m
         self.s = s
 
     def __repr__(self):
-        return f'<Time {self.h:2}:{self.m:02}:{self.s:02}>'
+        return f'<Tiempo {self.h:2}:{self.m:02}:{self.s:02}>'
 
     @property
     def h(self):
         return self._h
 
     @h.setter
+    @_int_requerido
     def h(self,value):
-        if not isinstance(value,int):
-            raise TypeError('Se requiere un valor int.')
         self._h = value
 
-# a = Time("Hola mundo",23,10)
-# a.h = "Hola mundo"
-a = Time(14,23,10)
-print('a: ',a.h,a.m,a.s)
+    @property
+    def m(self):
+        return self._m
 
-# b = Time(m=30)
+    @m.setter
+    @_int_requerido
+    def m(self,value):
+        self._m = value
+        self._h, self._m = _balance(self._h,self._m)
+
+    @property
+    def s(self):
+        return self._s
+
+    @s.setter
+    @_int_requerido
+    def s(self,value):
+        self._s = value
+        self._m,self._s = _balance(self._m,self._s)
+
+# a = Tiempo("Hola mundo",23,10)
+# a.h = "Hola mundo"
+# a = Tiempo(14,23,10)
+a = Tiempo(2,80,95)
+# print('a: ',a.h,a.m,a.s)
+print('a:',a)
+# b = Tiempo(m=30)
 # print('b: ',b.h,b.m,b.s)
 
 # print(a) 
-# Antes de definir __repr__: <__main__.Time object at 0x7f6ab4ad8f90>
-# Despues de definir __repr__: <Time 14:23:10>
+# Antes de definir __repr__: <__main__.Tiempo object at 0x7f6ab4ad8f90>
+# Despues de definir __repr__: <Tiempo 14:23:10>
 
 
