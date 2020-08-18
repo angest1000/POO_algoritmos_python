bb1a3fbcfe0f11f00eeb04fd4d5f5c70449ab44f
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-17 22:04:54 -0500

    Agregamos el archivo cadenas_char.py en el que estaremos trabajando las cadenas de caracteres del blog de recursospython y trabajamos la primer parte

---
+++ recursospython/cadenas_char.py
@@ -0,0 +1,15 @@
+# Cadenas de caracteres
+# Codigo extraido del siguiente enlace:
+# https://recursospython.com/guias-y-manuales/formateo-de-cadenas/
+
+
+nombre = "Juan"
+print("Tu nombre es: "+nombre)
+
+apellido = "Perez"
+sexo = "Masculino"
+print("Tu nombre es: " + nombre + ". Apellido: " + apellido + ". Sexo: " + sexo + ".")
+
+edad = 20
+print("Tu nombre es: " + nombre + " y tienes " + str(edad) + " años.")
+
