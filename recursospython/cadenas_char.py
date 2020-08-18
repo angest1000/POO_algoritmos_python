bfcca9402eb6bd12b76305b01af93a9bf11cad65
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-17 23:05:34 -0500

    Completamos el archivo de cadenas_char.py con todo lo que decía el blog

--- recursospython/cadenas_char.py
+++ recursospython/cadenas_char.py
@@ -2,41 +2,56 @@
 # Codigo extraido del siguiente enlace:
 # https://recursospython.com/guias-y-manuales/formateo-de-cadenas/
 
-#El metodo recomendado:
+# Un nuevo metodo python 3.6:
 nombre = "Juan"
 edad = 20
-print("Tu nombre es {0} y tienes {1} años.".format(nombre,edad))
-print("Tienes {1} años y te llamas {0}.".format(nombre,edad))
-print("{0} {1} {0} {1} {1} {0} {0}".format(nombre,edad))
+altura = 1.75
 
-#Se pueden omitir los numeros dentro de las llaves
-print("Tu nombre es {} y tienes {} años.".format(nombre,edad))
+print(f"Te llamas {nombre}, tienes {edad} años y mides {altura} metros.")
+print(f"Tu nombre al reves es {nombre[::-1]}.")
+print(f"Dentro de 5 años tendras {edad +5} años.")
 
-# Definiendo nombre a los valores:
-print("Tu nombre es {a} y tienes {b} años.".format(a=nombre,b=edad))
-
-print("Tu nombre es {0} y tienes {1} años. Mides {altura} metros.".format(nombre,edad,altura=1.75))
-
-for i in range(-3,4):
-    print("{0:+}".format(i))
+#Intentando hacer lo anterior con el metodo recomendado:
+print(f"Tu nombre es {nombre} y tienes {edad} años .".format(**locals()))
 
 for nombre in ("Juan","Pedro","Francisco"):
-    print("{0:>9}".format(nombre))
+    print(f"{nombre:>9}")
 
-#centrando los nombres:
-for nombre in ("Juan","Pedro","Francisco"):
-    print("{0:^9}".format(nombre))
+#El metodo recomendado:
+# nombre = "Juan"
+# edad = 20
+# print("Tu nombre es {0} y tienes {1} años.".format(nombre,edad))
+# print("Tienes {1} años y te llamas {0}.".format(nombre,edad))
+# print("{0} {1} {0} {1} {1} {0} {0}".format(nombre,edad))
 
-#Hexadecimal:
-for i in range(0,1000,100):
-    print("dec:{}, hex:0x{:X}".format(i,i))
+# #Se pueden omitir los numeros dentro de las llaves
+# print("Tu nombre es {} y tienes {} años.".format(nombre,edad))
 
-#Ambos son equivalentes:
-print("{:5}".format(10))
-print("{:{}}".format(10,5))
+# # Definiendo nombre a los valores:
+# print("Tu nombre es {a} y tienes {b} años.".format(a=nombre,b=edad))
 
-#Imprimiendo llaves:
-print("{{}}".format())
+# print("Tu nombre es {0} y tienes {1} años. Mides {altura} metros.".format(nombre,edad,altura=1.75))
+
+# for i in range(-3,4):
+#     print("{0:+}".format(i))
+
+# for nombre in ("Juan","Pedro","Francisco"):
+#     print("{0:>9}".format(nombre))
+
+# #centrando los nombres:
+# for nombre in ("Juan","Pedro","Francisco"):
+#     print("{0:^9}".format(nombre))
+
+# #Hexadecimal:
+# for i in range(0,1000,100):
+#     print("dec:{}, hex:0x{:X}".format(i,i))
+
+# #Ambos son equivalentes:
+# print("{:5}".format(10))
+# print("{:{}}".format(10,5))
+
+# #Imprimiendo llaves:
+# print("{{}}".format())
 
 
 # print("Tu nombre es %s " %nombre)
