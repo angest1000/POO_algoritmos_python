c64e62b1c9aeb265524cedf1057d0bd876238055
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-17 22:57:35 -0500

    Se agregó la parte de 'El método recomendado al archivo y se crearon 2 archivos que contienen ejemplos del uso del formato para crear figuras en consola

--- recursospython/cadenas_char.py
+++ recursospython/cadenas_char.py
@@ -2,9 +2,44 @@
 # Codigo extraido del siguiente enlace:
 # https://recursospython.com/guias-y-manuales/formateo-de-cadenas/
 
-
+#El metodo recomendado:
 nombre = "Juan"
-print("Tu nombre es %s " %nombre)
+edad = 20
+print("Tu nombre es {0} y tienes {1} años.".format(nombre,edad))
+print("Tienes {1} años y te llamas {0}.".format(nombre,edad))
+print("{0} {1} {0} {1} {1} {0} {0}".format(nombre,edad))
+
+#Se pueden omitir los numeros dentro de las llaves
+print("Tu nombre es {} y tienes {} años.".format(nombre,edad))
+
+# Definiendo nombre a los valores:
+print("Tu nombre es {a} y tienes {b} años.".format(a=nombre,b=edad))
+
+print("Tu nombre es {0} y tienes {1} años. Mides {altura} metros.".format(nombre,edad,altura=1.75))
+
+for i in range(-3,4):
+    print("{0:+}".format(i))
+
+for nombre in ("Juan","Pedro","Francisco"):
+    print("{0:>9}".format(nombre))
+
+#centrando los nombres:
+for nombre in ("Juan","Pedro","Francisco"):
+    print("{0:^9}".format(nombre))
+
+#Hexadecimal:
+for i in range(0,1000,100):
+    print("dec:{}, hex:0x{:X}".format(i,i))
+
+#Ambos son equivalentes:
+print("{:5}".format(10))
+print("{:{}}".format(10,5))
+
+#Imprimiendo llaves:
+print("{{}}".format())
+
+
+# print("Tu nombre es %s " %nombre)
 # %s: cadena
 # %d: entero
 # %f: flotante
@@ -13,46 +48,46 @@ print("Tu nombre es %s " %nombre)
 # sexo = "Masculino"
 # print("Tu nombre es: " + nombre + ". Apellido: " + apellido + ". Sexo: " + sexo + ".")
 
-edad = 20
-print("Tienes %d años." %edad)
+# edad = 20
+# print("Tienes %d años." %edad)
 
-altura = 1.75
-print("Tu nombre es %s. Tienes %d años. Mides %f metros." % (nombre,edad,altura))
+# altura = 1.75
+# print("Tu nombre es %s. Tienes %d años. Mides %f metros." % (nombre,edad,altura))
 
 
-# Para agregar el signo '%' en una cadena se pone de la siguiente manera:
-probabilidad = 70
-print("Hay una probabilidad de %d%%" %probabilidad)
+# # Para agregar el signo '%' en una cadena se pone de la siguiente manera:
+# probabilidad = 70
+# print("Hay una probabilidad de %d%%" %probabilidad)
 
-# Si utilizas %s pero no es str, se transforma en str:
-print("Tienes %s años. Mides %s metros." % (edad,altura))
+# # Si utilizas %s pero no es str, se transforma en str:
+# print("Tienes %s años. Mides %s metros." % (edad,altura))
 
-# Equivalente a :
-print("Tienes %s años. Mides %s metros." % (str(edad),str(altura)))
+# # Equivalente a :
+# print("Tienes %s años. Mides %s metros." % (str(edad),str(altura)))
 
-#Mostrando solo 2 decimales:
-a = 3.141592
-print("a es %.2f" % a)
+# #Mostrando solo 2 decimales:
+# a = 3.141592
+# print("a es %.2f" % a)
 
-altura = 1.75
-print("Mides %.6f metros." % altura)
+# altura = 1.75
+# print("Mides %.6f metros." % altura)
 
-#agregando + antes de d, los valores positivos tendran
-# su signo:
-for i in range(-3,4):
-    print("%+d" %i)
+# #agregando + antes de d, los valores positivos tendran
+# # su signo:
+# for i in range(-3,4):
+#     print("%+d" %i)
 
-#Imprimir hexadecimales:
-for i in range(15):
-    print("dec:%d hex:%x pref_ex:%#x manual: 0x%X" %(i,i,i,i))
+# #Imprimir hexadecimales:
+# for i in range(15):
+#     print("dec:%d hex:%x pref_ex:%#x manual: 0x%X" %(i,i,i,i))
 
-#Definiendo el largo del valor:
-for nombre in ("Juan","Pedro","Francisco"):
-    print("%9s" % nombre)
+# #Definiendo el largo del valor:
+# for nombre in ("Juan","Pedro","Francisco"):
+#     print("%9s" % nombre)
 
-for n in (1,10,100):
-    print("%3d" % n)
+# for n in (1,10,100):
+#     print("%3d" % n)
 
-# Añadiendo ceros:
-for n in (1,10,100):
-    print("%03d" % n)
+# # Añadiendo ceros:
+# for n in (1,10,100):
+#     print("%03d" % n)
---
+++ recursospython/char_piramide.py
@@ -0,0 +1,5 @@
+
+#Creando una piramide:
+n = 70
+for i in range(1,n,2):
+    print(" {0:^{1}}".format("A" * i,n))
