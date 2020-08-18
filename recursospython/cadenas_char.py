411c3b65232d60c1a7c2cf287d689eee2b269924
Angel Armando Estrada Engallo <angest1000@gmail.com>
2020-08-17 22:31:33 -0500

    Agregamos las partes de 'El método Original' al archivo de cadenas de texto

--- recursospython/cadenas_char.py
+++ recursospython/cadenas_char.py
@@ -4,12 +4,55 @@
 
 
 nombre = "Juan"
-print("Tu nombre es: "+nombre)
+print("Tu nombre es %s " %nombre)
+# %s: cadena
+# %d: entero
+# %f: flotante
 
-apellido = "Perez"
-sexo = "Masculino"
-print("Tu nombre es: " + nombre + ". Apellido: " + apellido + ". Sexo: " + sexo + ".")
+# apellido = "Perez"
+# sexo = "Masculino"
+# print("Tu nombre es: " + nombre + ". Apellido: " + apellido + ". Sexo: " + sexo + ".")
 
 edad = 20
-print("Tu nombre es: " + nombre + " y tienes " + str(edad) + " años.")
+print("Tienes %d años." %edad)
 
+altura = 1.75
+print("Tu nombre es %s. Tienes %d años. Mides %f metros." % (nombre,edad,altura))
+
+
+# Para agregar el signo '%' en una cadena se pone de la siguiente manera:
+probabilidad = 70
+print("Hay una probabilidad de %d%%" %probabilidad)
+
+# Si utilizas %s pero no es str, se transforma en str:
+print("Tienes %s años. Mides %s metros." % (edad,altura))
+
+# Equivalente a :
+print("Tienes %s años. Mides %s metros." % (str(edad),str(altura)))
+
+#Mostrando solo 2 decimales:
+a = 3.141592
+print("a es %.2f" % a)
+
+altura = 1.75
+print("Mides %.6f metros." % altura)
+
+#agregando + antes de d, los valores positivos tendran
+# su signo:
+for i in range(-3,4):
+    print("%+d" %i)
+
+#Imprimir hexadecimales:
+for i in range(15):
+    print("dec:%d hex:%x pref_ex:%#x manual: 0x%X" %(i,i,i,i))
+
+#Definiendo el largo del valor:
+for nombre in ("Juan","Pedro","Francisco"):
+    print("%9s" % nombre)
+
+for n in (1,10,100):
+    print("%3d" % n)
+
+# Añadiendo ceros:
+for n in (1,10,100):
+    print("%03d" % n)
